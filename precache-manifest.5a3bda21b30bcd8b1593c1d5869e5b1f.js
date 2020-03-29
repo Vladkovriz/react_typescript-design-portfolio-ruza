98f8239053a78931e3383fee2f9b3ebf6056c5f1
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2020-03-29 21:54:50 +0300

    Updates

---
+++ precache-manifest.5a3bda21b30bcd8b1593c1d5869e5b1f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6029ba6f5d0963b53e67f6a764a589b",
+    "url": "/react_typescript-design-portfolio-ruza/index.html"
+  },
+  {
+    "revision": "497bed12e95bc01afff2",
+    "url": "/react_typescript-design-portfolio-ruza/static/css/main.3746942f.chunk.css"
+  },
+  {
+    "revision": "3edfbfd2573c6f24ca62",
+    "url": "/react_typescript-design-portfolio-ruza/static/js/2.781bb2ea.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/react_typescript-design-portfolio-ruza/static/js/2.781bb2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497bed12e95bc01afff2",
+    "url": "/react_typescript-design-portfolio-ruza/static/js/main.b5d63f28.chunk.js"
+  },
+  {
+    "revision": "f7d936759a414af513f5",
+    "url": "/react_typescript-design-portfolio-ruza/static/js/runtime-main.b2ac1100.js"
+  },
+  {
+    "revision": "952e80090b48a1d931908ff1c30452d2",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/AllWorkIcon.952e8009.svg"
+  },
+  {
+    "revision": "e95fe7d837f5c0b5da499c7bd5658456",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Arrow.e95fe7d8.svg"
+  },
+  {
+    "revision": "4f803bf6a6f24ffbbb1316dc3ca27b56",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Black_In.4f803bf6.svg"
+  },
+  {
+    "revision": "d8d6499f1d3d29685ed9f1b3b7400323",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Black_Instagram.d8d6499f.svg"
+  },
+  {
+    "revision": "5dbe5705da7e64eae4fde92725d81386",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Black_Telegram.5dbe5705.svg"
+  },
+  {
+    "revision": "228740d06c5ff2c0ede45ec3b7b206fb",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Black_Viber.228740d0.svg"
+  },
+  {
+    "revision": "971ec66505c5657ce3a234732d127b1b",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Black_WA.971ec665.svg"
+  },
+  {
+    "revision": "8bba76c2d9cbfafb18b79360338ca113",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/ContactInfo.8bba76c2.svg"
+  },
+  {
+    "revision": "a8fc9d199639c3127967570a4797e5c8",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Download-B.a8fc9d19.svg"
+  },
+  {
+    "revision": "5208b8e34969e99dcf83cdf8c08c3230",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Pen-B.5208b8e3.svg"
+  },
+  {
+    "revision": "a05263d90b0f1bcf4d1e5c1b4fe78a52",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/PortfolioRuza.a05263d9.jpeg"
+  },
+  {
+    "revision": "e3391e279aae41022d9dec0798ad7bb4",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle1.e3391e27.png"
+  },
+  {
+    "revision": "aa6c5e2b8cf5f5fd169e09552b092385",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle4.aa6c5e2b.png"
+  },
+  {
+    "revision": "9bc1723b475ea2b2d8a00437908e69cf",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle4_1.9bc1723b.png"
+  },
+  {
+    "revision": "4c9e97b1224ea03add760a323ad85802",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle5.4c9e97b1.png"
+  },
+  {
+    "revision": "e7b8471aeaf4b8e9c44bb3871c9beb55",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle5_1.e7b8471a.png"
+  },
+  {
+    "revision": "665de4abca671fe6dc6927bd68b98c09",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Rectangle5_2.665de4ab.png"
+  },
+  {
+    "revision": "7631931b7b43bc2ff9ce317afcbbdad7",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/Ruza.7631931b.png"
+  },
+  {
+    "revision": "4998e06800f4a68cd99491888f4e8ec6",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/White_In.4998e068.svg"
+  },
+  {
+    "revision": "f8f77790cd11c297f1cc165bef7891ca",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/White_Instagram.f8f77790.svg"
+  },
+  {
+    "revision": "fd8b00c2b2bc3fa42cdfb4328a386569",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/White_Telegram.fd8b00c2.svg"
+  },
+  {
+    "revision": "9775b4a63c675666410e611033742552",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/White_Viber.9775b4a6.svg"
+  },
+  {
+    "revision": "a08385259d78767267c3450a62bbd521",
+    "url": "/react_typescript-design-portfolio-ruza/static/media/White_WA.a0838525.svg"
+  }
+]);
